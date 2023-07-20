353d36f185e0939fedc57b7ea59aa1f42608268a
Mikayel5464 <miqomuradyan186@gmail.com>
2023-07-20 17:42:26 +0400

    Homeworks

---
+++ Homeworks/Homework3/leap_year.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+
+	do
+	{
+		printf("Enter the year: ");
+		scanf("%d", &a);
+	}
+	while (a < 0);
+
+	if (a % 4 != 0)
+		printf("This year is ordinary \n");
+	else if (a % 100 == 0 & a % 400 != 0)
+		printf("This year is ordinary \n");
+	else
+		printf("This year is leap-year! \n");
+
+	return 0;
+}
+
